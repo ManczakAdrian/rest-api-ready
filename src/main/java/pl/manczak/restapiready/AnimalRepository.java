6aec400c01457ef9a960828646c2caa75ed01d9d
Manczak Adrian <manczakadrian@gmail.com>
2020-10-20 01:37:20 +0200

    Initial commit

---
+++ src/main/java/pl/manczak/restapiready/AnimalRepository.java
@@ -0,0 +1,13 @@
+package pl.manczak.restapiready;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+
+@RepositoryRestResource(collectionResourceRel = "animal", path = "animal")
+public interface AnimalRepository extends PagingAndSortingRepository<Animal, Long> {
+
+
+}
